e7eed78293699daabbd8b36daee1f8ade2ded92b
exogenesys <hrshgtm9@gmail.com>
2017-12-27 19:07:27 +0530

    server - push, pull func

--- server/main.go
+++ server/main.go
@@ -13,11 +13,14 @@ import (
     "log"
     "net/http"
     "strings"
-
+    
     "github.com/dgrijalva/jwt-go"
     "github.com/gorilla/context"
     "github.com/gorilla/mux"
     "github.com/mitchellh/mapstructure"
+
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
 type User struct {
@@ -33,11 +36,46 @@ type Exception struct {
     Message string `json:"message"`
 }
 
+type Snippet struct {
+    Name string `json:"name"`
+    Desc string `json:"desc"`
+    Code string `json:"code"`
+}
+
+type Response struct {
+    Success bool `json:"success"`
+    Data Snippet `json:"data"`
+    Err string `json:"err"`
+}
+
+func (s *Snippet) String() string {
+    return fmt.Sprintf("Name: %v | Desc: %v | Code: %v", s.Name, s.Desc, s.Code)
+}
+
+var session *mgo.Session
+
+func MongoConnect() (*mgo.Session){
+
+    // session, err := mgo.Dial("mongodb://<dbuser>:<dbpassword>@ds231987.mlab.com:31987/sniper")
+    session, err := mgo.Dial("mongodb://american-sniper:economyoverecology@ds231987.mlab.com:31987/sniper")
+    if err != nil {
+        panic(err)
+    } else {
+        fmt.Println("Mon is go")
+        // defer session.Close()
+    }
+
+    return session
+}
 
 
 func main() {
+
     router := mux.NewRouter()
+    session = MongoConnect()
     fmt.Println("Starting the application...")
+    router.HandleFunc("/push", PushSnippet).Methods("POST")
+    router.HandleFunc("/pull", PullSnippet).Methods("GET")
     router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
     router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
     router.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
@@ -83,6 +121,47 @@ func ProtectedEndpoint(w http.ResponseWriter, req *http.Request) {
         json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
     }
 }
+
+
+
+func PushSnippet(w http.ResponseWriter, req *http.Request) {
+
+    var snippet Snippet
+    _ = json.NewDecoder(req.Body).Decode(&snippet)
+
+    fmt.Println(snippet)
+
+    c := session.DB("sniper").C("sniper-snippets")
+    err := c.Insert(&snippet)
+    if err != nil {
+        json.NewEncoder(w).Encode(Response{Success: false, Err: err.Error()})            
+        fmt.Println("Error:", err)
+    } else {
+        json.NewEncoder(w).Encode(Response{Success: true})
+        log.Println("Snippet saved")
+    }
+}
+
+func PullSnippet(w http.ResponseWriter, req *http.Request) {
+
+    params := req.URL.Query()
+    fmt.Println(params["name"][0])
+
+    var snippet Snippet
+
+    c := session.DB("sniper").C("sniper-snippets")
+
+    err := c.Find(bson.M{"name": params["name"][0]}).One(&snippet)
+    if err != nil {
+        // log.Fatal(err)
+        json.NewEncoder(w).Encode(Response{Success: false, Err: err.Error()})
+        fmt.Println("Error:", err)
+    } else {
+        fmt.Println("Snippet:", snippet)
+        json.NewEncoder(w).Encode(Response{Success: true, Data: snippet})
+    }
+}
+
 
 func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
     return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
