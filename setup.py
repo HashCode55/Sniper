d878b9586df2ddee96a4fa16c7b9837ac70d5329
hashcode55 <blackdragon0909@gmail.com>
2018-01-08 14:28:46 +0530

    setuppy fix

--- setup.py
+++ setup.py
@@ -6,15 +6,14 @@ data  : 09/12/2017
 """
 
 import setuptools
-from .sniper.version import Version
+from sniper.version import Version
 
 setuptools.setup(name='pysniper',
                  version=Version('1.0.0').number,
                  description='Sniper is a clould enabled, terminal based easy to use snippet manager.',
                  long_description=open('README.md').read().strip(),
                  author='HashCode55',
-                 author_email='mehul.ahuja09@gmail.com',
-                 url='http://path-to-my-packagename',
+                 author_email='mehul.ahuja09@gmail.com',                 
                  py_modules=['sniper'],
                  install_requires=[
                      'Click',
